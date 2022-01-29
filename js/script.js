194e59071dca9039096002a98bf0f4f4450b381e
Karine-Bauch <karine.bauch.web@gmail.com>
2022-01-29 19:17:32 +0100

    Dark / Light Mode added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const switchEl = document.querySelector('.switch');
+// console.log(switchEl);
+const titleEl = document.querySelector('.banniere-titre');
+// console.log(titleEl);
+
+lightDarkMode = () => {
+    const mode = document.querySelector("link[href='./css/style-light.css']");
+
+    if(mode) {
+        document.querySelector("link[href='./css/style-light.css']").href = "./css/style-dark.css";
+        titleEl.innerText = "DARK MODE";
+    } else {
+        document.querySelector("link[href='./css/style-dark.css']").href = "./css/style-light.css";
+        titleEl.innerText = "LIGHT MODE";
+    }
+}
+
+switchEl.addEventListener('click', lightDarkMode);
